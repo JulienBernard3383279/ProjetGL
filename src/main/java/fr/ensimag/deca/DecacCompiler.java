50c904a92c5999cc1fec95303f91a517225076a4
Guillaume Milan <guillaume.milan@ensimag.grenoble-inp.fr>
2017-01-16 11:41:45 +0100

    ajout des test automatique de codegen

--- src/main/java/fr/ensimag/deca/DecacCompiler.java
+++ src/main/java/fr/ensimag/deca/DecacCompiler.java
@@ -203,7 +203,7 @@ public class DecacCompiler {
             } else if (compilerOptions.getVerif() ) {
                 AbstractProgram prog = doLexingAndParsing(sourceFile, out);
                 prog.verifyProgram(this);
-                prog.prettyPrint();
+                prog.prettyPrint(out);
                 return false ;
             }
             return doCompile(sourceFile, destFile, out, err);
